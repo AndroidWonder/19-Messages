b932ea658d22d791accdac742819d967d437c627
ClassroomAccount <jpepe44@gmail.com>
2020-04-03 10:10:23 -0400

    update rev

--- app/src/main/java/com/course/example/messages/Messages.java
+++ app/src/main/java/com/course/example/messages/Messages.java
@@ -26,6 +26,7 @@ public class Messages extends Activity {
 	Handler handler = new Handler() {
 		
 		public void handleMessage(Message msg) {
+
 			bar.incrementProgressBy(5);
 			
 			if (bar.getProgress() == bar.getMax()){
